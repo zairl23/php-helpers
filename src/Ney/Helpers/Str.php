99e0f1063bbbe338ead62c5b155f48a4ab362f1d
zairl23 <756127792@qq.com>
2016-08-06 18:39:47 +0800

    Add functions for Str class

--- src/Ney/Helpers/Str.php
+++ src/Ney/Helpers/Str.php
@@ -5,12 +5,31 @@ use Illuminate\Support\Str as IlluminateStr;
 class Str extends IlluminateStr 
 {
 	/**
-	 * Hide the string by mask strings
+	 * Hide the string by mask strings at the end of the string
 	 *
 	 */
-	public function hide_with($str, $mask='*', $times=3)
+	public function hide_with_end($str, $mask='*', $times=3)
 	{
-		
+		return substr_replace($str, str_repeat($mask, $times), 0-$times, $times);
+	}
+
+	/**
+	 * Hide the string by mask strings at the head of the string
+	 *
+	 */
+	public function hide_with_head($str, $mask='*', $times=3)
+	{
+		return substr_replace($str, str_repeat($mask, $times), 0, $times);
+	}
+
+	/**
+	 * Hide the string by mask strings at the center of the string
+	 *
+	 */
+	public function hide_with_center($str, $mask='*', $times=3)
+	{
+		$index = ceil(mb_strlen($str) /2) - ceil(mb_strlen($times) / 2);
+		return substr_replace($str, str_repeat($mask, $times), $index, $times);
 	}
 
 }
--- tests/StrTest.php
+++ tests/StrTest.php
@@ -4,9 +4,38 @@ use Ney\Helpers\Str;
 
 class StrTest extends PHPUnit_Framework_TestCase 
 {
-	public function test_hide_with()
+	public function test_hide_with_end()
 	{
-		
+
+		$this->assertEquals('t***', Str::hide_with_end('test'));
+		$this->assertEquals('***', Str::hide_with_end('t'));
+		$this->assertEquals('***', Str::hide_with_end('tt'));
+		$this->assertEquals('***', Str::hide_with_end('ttt'));
+		$this->assertEquals('+++', Str::hide_with_end('ttt', '+'));
+		$this->assertEquals('t++', Str::hide_with_end('ttt', '+', 2));
+		// $this->assertEquals('t++', Str::hide_with_end('我是谁', '+', 2));
+	}
+
+	public function test_hide_with_head()
+	{
+
+		$this->assertEquals('***t', Str::hide_with_head('test'));
+		$this->assertEquals('***', Str::hide_with_head('t'));
+		$this->assertEquals('***', Str::hide_with_head('tt'));
+		$this->assertEquals('***', Str::hide_with_head('ttt'));
+
+	}
+
+
+	public function test_hide_with_center()
+	{
+
+		$this->assertEquals('t***', Str::hide_with_center('test'));
+		$this->assertEquals('t-st', Str::hide_with_center('test', '-', 1));
+		$this->assertEquals('tt***t', Str::hide_with_center('tttttt'));
+		// $this->assertEquals('***', Str::hide_with_center('tt'));
+		// $this->assertEquals('***', Str::hide_with_center('ttt'));
+
 	}
 
 
