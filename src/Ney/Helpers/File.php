5617da5f25a60555b40f5fba8b49e4352fda97d8
zairl23 <756127792@.com>
2017-11-05 20:01:03 +0800

    Add new function of File class

--- src/Ney/Helpers/File.php
+++ src/Ney/Helpers/File.php
@@ -24,4 +24,45 @@ class File {
 		}
 	}
 
+	/**
+	 * Export the datas to csv file for downloading
+	 *
+	 * @param string $fileName
+	 * @param array  $datas
+	 * @param string $tableHead
+	 * @return file stream
+	 * @link https://mp.weixin.qq.com/s?timestamp=1509880911&src=3&ver=1&signature=ryMORtyG6dZ-9u6JNVMXgYYkDDuEYMmmkK8tZIoSvbLEWOQj7ZRwhO5KIbBuM3HDLYvKhFB-sHDz2S-qwSjlA7Pus-AGOvuJi*4mfvIGOGQQkTJz4v2YmwxWVzki2DMU327o-bsaIk0DbMdyyqaQENFFnkNW5PYi61jfQm3k374=
+	 */
+	public static function exportcsv($fileName, $datas, $tableHead)
+	{
+
+			set_time_limit(0);
+			ini_set('memory_limit', '512M');
+
+			$output = fopen('php://output', 'w') or die("can't open php://output");
+
+			header("Content-Type: application/csv");
+			header("Content-Disposition: attachment; filename=$fileName.csv");
+			header('Expires: 0');
+  		header('Cache-Control: must-revalidate');
+  		header('Pragma: public');
+
+			//Windows下使用BOM来标记文本文件的编码方式,防止中文乱码
+			fwrite($output,chr(0xEF).chr(0xBB).chr(0xBF));
+
+			fputcsv($output, $tableHead);
+
+			foreach ($datas as $data) {
+					$data = (array) $data;
+					fputcsv($output, array_values($data));
+			}
+
+			unset($datas);
+			ob_flush();
+			flush();
+
+			fclose($output) or die("can't close php://output");
+
+	}
+
 }
