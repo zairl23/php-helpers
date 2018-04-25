c3afc7494ab2fc728513c935500f8b7c2b3c6966
zairl23 <756127792@.com>
2018-04-25 20:37:22 +0800

    Add is_version_code and is_version_number functions

--- src/Ney/Helpers/Str.php
+++ src/Ney/Helpers/Str.php
@@ -4,96 +4,117 @@ use Illuminate\Support\Str as IlluminateStr;
 
 class Str extends IlluminateStr
 {
-	/**
-	 * Hide the string by mask strings at the end of the string
-	 *
-	 * @param $str
-	 * @param $mask
-	 * @param $times
-	 * @return string
-	 */
-	public static function hide_with_end($str, $mask='*', $times=3)
-	{
-		return substr_replace($str, str_repeat($mask, $times), 0-$times, $times);
-	}
+    /**
+     * Hide the string by mask strings at the end of the string
+     *
+     * @param $str
+     * @param $mask
+     * @param $times
+     * @return string
+     */
+    public static function hide_with_end($str, $mask = '*', $times = 3)
+    {
+        return substr_replace($str, str_repeat($mask, $times), 0 - $times, $times);
+    }
 
-	/**
-	 * Hide the string by mask strings at the head of the string
-	 *
-	 * @param $str
-	 * @param $mask
-	 * @param $times
-	 * @return string
-	 */
-	public static function hide_with_head($str, $mask='*', $times=3)
-	{
-		return substr_replace($str, str_repeat($mask, $times), 0, $times);
-	}
+    /**
+     * Hide the string by mask strings at the head of the string
+     *
+     * @param $str
+     * @param $mask
+     * @param $times
+     * @return string
+     */
+    public static function hide_with_head($str, $mask = '*', $times = 3)
+    {
+        return substr_replace($str, str_repeat($mask, $times), 0, $times);
+    }
 
-	/**
-	 * Hide the string by mask strings at the center of the string
-	 *
-	 * @param $str
-	 * @param $mask
-	 * @param $times
-	 * @return string
-	 */
-	public static function hide_with_center($str, $mask='*', $times=3)
-	{
-		$index = ceil(mb_strlen($str) /2) - ceil(mb_strlen($times) / 2);
-		return substr_replace($str, str_repeat($mask, $times), $index, $times);
-	}
+    /**
+     * Hide the string by mask strings at the center of the string
+     *
+     * @param $str
+     * @param $mask
+     * @param $times
+     * @return string
+     */
+    public static function hide_with_center($str, $mask = '*', $times = 3)
+    {
+        $index = ceil(mb_strlen($str) / 2) - ceil(mb_strlen($times) / 2);
+        return substr_replace($str, str_repeat($mask, $times), $index, $times);
+    }
 
-	/**
-	 * Create an unique string and the string's length equal 32
-	 *
-	 * @return string
-	 */
-	public static function create_uniq_key()
-	{
-		return md5(microtime().rand());
-	}
+    /**
+     * Create an unique string and the string's length equal 32
+     *
+     * @return string
+     */
+    public static function create_uniq_key()
+    {
+        return md5(microtime() . rand());
+    }
 
-	/**
-	 * Check the given string is a url format
-	 *
-	 * @return boolean
-	 */
-	public static function is_url($str)
-	{
-			return preg_match("^http(s)?:\/\/.+", $str) === 1;
-	}
+    /**
+     * Check the given string is a url format
+     *
+     * @return boolean
+     */
+    public static function is_url($str)
+    {
+        return preg_match("^http(s)?:\/\/.+", $str) === 1;
+    }
 
-  /**
-		* Check the given string is a blank string
-		*
-		* @return boolean
-		*/
-	public static function is_blank($str)
-	{
-		 return preg_match("/^[\s]*$/", $str) === 1;
-	}
+    /**
+     * Check the given string is a blank string
+     *
+     * @return boolean
+     */
+    public static function is_blank($str)
+    {
+        return preg_match("/^[\s]*$/", $str) === 1;
+    }
 
-	/**
-		* Check the given string is a chinese words
-		*
-		* @return boolean
-		*/
+    /**
+     * Check the given string is a chinese words
+     *
+     * @return boolean
+     */
 	// preg_match("/^[\x{4e00}-\x{9fa5}a-zA-z0-9]{1,}([\x{4e00}-\x{9fa5}a-zA-Z0-9_?%&*,;= ]){1,}$/u
-	public static function is_chinese($str)
-	{
-		 return preg_match("/^[\x{4e00}-\x{9fa5}]$/u", $str) === 1;
-	}
+    public static function is_chinese($str)
+    {
+        return preg_match("/^[\x{4e00}-\x{9fa5}]$/u", $str) === 1;
+    }
 
-	/**
-		* Check the given string contain chinese string
-		*
-		* @return boolean
-		*/
-	public static function contain_chinese($str)
-	{
-		 return preg_match("/[\x{4e00}-\x{9fa5}]+/u", $str) === 1;
-	}
+    /**
+     * Check the given string contain chinese string
+     *
+     * @return boolean
+     */
+    public static function contain_chinese($str)
+    {
+        return preg_match("/[\x{4e00}-\x{9fa5}]+/u", $str) === 1;
+    }
 
+    /**
+     * Check the given string is version code format
+     *
+     * @param string $str
+     * @return boolean
+     */
+    public static function is_version_code($str)
+    {
+        return preg_match("/^[1-9]{1}[0-9]*$/", $str) === 1;
+    }
+
+    /**
+     * Check the given string is version number format
+     *
+     * @param  string $str
+     * @return boolean
+     */
+    public static function is_version_number($str)
+    {
+        return preg_match("/^[0-9]{1}.[0-9.]*[0-9]$/", $str) === 1;
+    }
 
 }
--- tests/StrTest.php
+++ tests/StrTest.php
@@ -4,65 +4,79 @@ use Ney\Helpers\Str;
 
 class StrTest extends PHPUnit_Framework_TestCase
 {
-	public function test_hide_with_end()
-	{
+    public function test_hide_with_end()
+    {
 
-		$this->assertEquals('t***', Str::hide_with_end('test'));
-		$this->assertEquals('***', Str::hide_with_end('t'));
-		$this->assertEquals('***', Str::hide_with_end('tt'));
-		$this->assertEquals('***', Str::hide_with_end('ttt'));
-		$this->assertEquals('+++', Str::hide_with_end('ttt', '+'));
-		$this->assertEquals('t++', Str::hide_with_end('ttt', '+', 2));
+        $this->assertEquals('t***', Str::hide_with_end('test'));
+        $this->assertEquals('***', Str::hide_with_end('t'));
+        $this->assertEquals('***', Str::hide_with_end('tt'));
+        $this->assertEquals('***', Str::hide_with_end('ttt'));
+        $this->assertEquals('+++', Str::hide_with_end('ttt', '+'));
+        $this->assertEquals('t++', Str::hide_with_end('ttt', '+', 2));
 		// $this->assertEquals('t++', Str::hide_with_end('我是谁', '+', 2));
-	}
+    }
 
-	public function test_hide_with_head()
-	{
+    public function test_hide_with_head()
+    {
 
-		$this->assertEquals('***t', Str::hide_with_head('test'));
-		$this->assertEquals('***', Str::hide_with_head('t'));
-		$this->assertEquals('***', Str::hide_with_head('tt'));
-		$this->assertEquals('***', Str::hide_with_head('ttt'));
+        $this->assertEquals('***t', Str::hide_with_head('test'));
+        $this->assertEquals('***', Str::hide_with_head('t'));
+        $this->assertEquals('***', Str::hide_with_head('tt'));
+        $this->assertEquals('***', Str::hide_with_head('ttt'));
 
-	}
+    }
 
 
-	public function test_hide_with_center()
-	{
+    public function test_hide_with_center()
+    {
 
-		$this->assertEquals('t***', Str::hide_with_center('test'));
-		$this->assertEquals('t-st', Str::hide_with_center('test', '-', 1));
-		$this->assertEquals('tt***t', Str::hide_with_center('tttttt'));
+        $this->assertEquals('t***', Str::hide_with_center('test'));
+        $this->assertEquals('t-st', Str::hide_with_center('test', '-', 1));
+        $this->assertEquals('tt***t', Str::hide_with_center('tttttt'));
 		// $this->assertEquals('***', Str::hide_with_center('tt'));
 		// $this->assertEquals('***', Str::hide_with_center('ttt'));
 
-	}
+    }
 
-	public function test_create_uniq_key()
-	{
-		$this->assertEquals(32, strlen(Str::create_uniq_key()));
-	}
+    public function test_create_uniq_key()
+    {
+        $this->assertEquals(32, strlen(Str::create_uniq_key()));
+    }
 
-	public function test_is_blank()
-	{
-		 $this->assertTrue(Str::is_blank("\n"));
-		 $this->assertTrue(Str::is_blank("\t"));
-	}
+    public function test_is_blank()
+    {
+        $this->assertTrue(Str::is_blank("\n"));
+        $this->assertTrue(Str::is_blank("\t"));
+    }
 
-	public function test_is_chinese()
-	{
-		 $this->assertTrue(Str::is_chinese('我'));
-		 $this->assertFalse(Str::is_chinese('test'));
-		 $this->assertFalse(Str::is_chinese('我是neo'));
-	}
+    public function test_is_chinese()
+    {
+        $this->assertTrue(Str::is_chinese('我'));
+        $this->assertFalse(Str::is_chinese('test'));
+        $this->assertFalse(Str::is_chinese('我是neo'));
+    }
 
-	public function test_contain_chinese()
-	{
-			$this->assertTrue(Str::contain_chinese('我'));
-			$this->assertFalse(Str::contain_chinese('test'));
-			$this->assertTrue(Str::contain_chinese('我是neo'));
-			$this->assertTrue(Str::contain_chinese('neo是我'));
-	}
+    public function test_contain_chinese()
+    {
+        $this->assertTrue(Str::contain_chinese('我'));
+        $this->assertFalse(Str::contain_chinese('test'));
+        $this->assertTrue(Str::contain_chinese('我是neo'));
+        $this->assertTrue(Str::contain_chinese('neo是我'));
+    }
 
+    public function test_is_version_code()
+    {
+        $this->assertTrue(Str::is_version_code("3"));
+        $this->assertFalse(Str::is_version_code("0"));
+        $this->assertFalse(Str::is_version_code("kl"));
+    }
+
+    public function test_is_version_number()
+    {
+        $this->assertTrue(Str::is_version_number("3.4.5"));
+
+        $this->assertFalse(Str::is_version_number("a"));
+
+    }
 
 }
